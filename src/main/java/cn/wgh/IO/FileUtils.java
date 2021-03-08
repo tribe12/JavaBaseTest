1c0cb7f5a2ad98542fae2837aac2f79469e44085
tribe12 <tribe12wgh@163.com>
2021-03-08 15:25:13 +0800

    My FileUtils

--- src/main/java/cn/wgh/IO/FileUtils.java
+++ src/main/java/cn/wgh/IO/FileUtils.java
@@ -5,13 +5,17 @@ import java.io.IOException;
 
 /**
  * 文件，文件夹复制工具
- *
+ * <p>
  * Created by wgh on 2021/3/4.
  */
 public class FileUtils {
 
     public static void copyAllFileToDirectory(File src, File destDir) {
-        long copyCount = 0;
+        long copyCount = copyAllFileToDirectory(src, destDir, 0l);
+        System.out.println("copy file count:" + copyCount);
+    }
+
+    private static long copyAllFileToDirectory(File src, File destDir, long copyCount) {
         System.out.println(src.getName());
         if (src.isDirectory()) {
             // 获取路径下的所有文件
@@ -19,7 +23,7 @@ public class FileUtils {
             for (int i = 0; i < files.length; i++) {
                 // 如果还是文件夹，递归获取里面的文件、文件夹
                 if (files[i].isDirectory()) {
-                    copyAllFileToDirectory(files[i], destDir);
+                    copyCount = copyAllFileToDirectory(files[i], destDir, copyCount);
                 } else {
                     // 如果是文件就直接拷贝
                     try {
@@ -39,6 +43,6 @@ public class FileUtils {
                 e.printStackTrace();
             }
         }
-        System.out.println("copy file count:"+copyCount);
+        return copyCount;
     }
 }
